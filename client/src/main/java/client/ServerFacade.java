441e4b627b09eb2ebdac4d0f4dbea765b8260787
Joon Shin <shinjoonhee1998@gmail.com>
2026-03-24 23:58:18 -0600

    register, login, makeRequest implemented

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -14,11 +14,43 @@ public class ServerFacade {
     }
 
     public RegisterResult register(RegisterRequest request) throws Exception {
-        return null;
+        return makeRequest("POST", "/user", null, request, RegisterResult.class);
     }
 
     public LoginResult login(LoginRequest request) throws Exception {
-        return null;
+        return makeRequest("POST", "/session", null, request, LoginResult.class);
+    }
+
+    private <T> T makeRequest(String method, String path, String authToken, Object requestBody, Class<T> responseClass) throws Exception {
+        URL url = new URI(serverUrl + path).toURL();
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod(method);
+        http.setDoOutput(requestBody != null);
+
+        if (authToken != null) {
+            http.addRequestProperty("Authorization", authToken);
+        }
+
+        if (requestBody != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            try (OutputStream os = http.getOutputStream()) {
+                os.write(new Gson().toJson(requestBody).getBytes());
+            }
+        }
+
+        http.connect();
+        
+        if (http.getResponseCode() >= 400) {
+            throw new Exception("Error: " + http.getResponseCode());
+        }
+
+        try (InputStream respBody = http.getInputStream()) {
+            InputStreamReader reader = new InputStreamReader(respBody);
+            if (responseClass == null) {
+                return null;
+            }
+            return new Gson().fromJson(reader, responseClass);
+        }
     }
 
     public void logout(String authToken) throws Exception {
