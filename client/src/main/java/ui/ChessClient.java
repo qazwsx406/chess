067e74c8194f91dfc68153fd712961ef708fcb3e
Joon Shin <shinjoonhee1998@gmail.com>
2026-04-09 07:48:30 -0600

    client

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,17 +1,28 @@
 package ui;
 
 import client.ServerFacade;
+import client.websocket.NotificationHandler;
+import client.websocket.WebSocketCommunicator;
 import model.GameData;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ErrorMessage;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ChessClient {
+public class ChessClient implements NotificationHandler {
     private final ServerFacade facade;
     private final String serverUrl;
     private State state = State.LOGGED_OUT;
     private String authToken = null;
     private Map<Integer, Integer> gameIdMap = new HashMap<>();
+    private WebSocketCommunicator ws = null;
+    private Integer currentGameId = null;
+    private String playerColor = null;
 
     public ChessClient(String serverUrl) {
         this.serverUrl = serverUrl;
@@ -27,7 +38,7 @@ public class ChessClient {
     public String eval(String input) {
         try {
             var tokens = input.split(" ");
-            var cmd = (tokens.length > 0) ? tokens[0].toLowerCase() : "help";
+            var cmd = (tokens.length > 0) ? tokens[0].toLowerCase() : "help";      
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
                 case "help" -> help();
@@ -38,11 +49,34 @@ public class ChessClient {
                 case "list" -> listGames();
                 case "play" -> joinGame(params);
                 case "observe" -> observeGame(params);
+                case "redraw" -> redrawBoard();
+                case "leave" -> leave();
                 case "quit" -> "quit";
                 default -> "Unknown command. Type Help to see available commands.";
             };
         } catch (Exception e) {
             return e.getMessage();
+        }
+    }
+
+    @Override
+    public void notify(ServerMessage notification) {
+        switch (notification.getServerMessageType()) {
+            case LOAD_GAME -> {
+                LoadGameMessage loadMessage = (LoadGameMessage) notification;
+                System.out.println("\n" + BoardDrawer.draw(playerColor != null ? playerColor : "WHITE"));
+                System.out.print("[GAMEPLAY] >>> ");
+            }
+            case NOTIFICATION -> {
+                NotificationMessage notifMessage = (NotificationMessage) notification;
+                System.out.println("\n" + notifMessage.getMessage());
+                System.out.print("[GAMEPLAY] >>> ");
+            }
+            case ERROR -> {
+                ErrorMessage errorMessage = (ErrorMessage) notification;
+                System.out.println("\n" + errorMessage.getErrorMessage());
+                System.out.print("[GAMEPLAY] >>> ");
+            }
         }
     }
 
@@ -56,7 +90,7 @@ public class ChessClient {
         var res = facade.register(new service.RegisterRequest(params[0], params[1], params[2]));
         state = State.LOGGED_IN;
         authToken = res.authToken();
-        return "Successfully registered and logged in as " + res.username() + ".";
+        return "Successfully registered and logged in as " + res.username() + "."; 
     }
 
     private String login(String[] params) throws Exception {
@@ -66,7 +100,7 @@ public class ChessClient {
         if (params.length != 2) {
             throw new Exception("Expected: login <USERNAME> <PASSWORD>");
         }
-        var res = facade.login(new service.LoginRequest(params[0], params[1]));
+        var res = facade.login(new service.LoginRequest(params[0], params[1]));    
         state = State.LOGGED_IN;
         authToken = res.authToken();
         return "Logged in as " + res.username() + ".";
@@ -84,12 +118,12 @@ public class ChessClient {
 
     private String createGame(String[] params) throws Exception {
         if (state == State.LOGGED_OUT) {
-            throw new Exception("You must be logged in to create a game.");
+            throw new Exception("You must be logged in to create a game.");        
         }
         if (params.length != 1) {
             throw new Exception("Expected: create <NAME>");
         }
-        facade.createGame(authToken, new service.CreateGameRequest(params[0]));
+        facade.createGame(authToken, new service.CreateGameRequest(params[0]));    
         return "Created game " + params[0] + ".";
     }
 
@@ -103,7 +137,7 @@ public class ChessClient {
         int i = 1;
         for (var game : res.games()) {
             gameIdMap.put(i, game.gameID());
-            sb.append(i).append(". ").append(game.gameName()).append("\n");
+            sb.append(i).append(". ").append(game.gameName()).append("\n");        
             sb.append("   White: ").append(game.whiteUsername() != null ? game.whiteUsername() : "Empty").append("\n");
             sb.append("   Black: ").append(game.blackUsername() != null ? game.blackUsername() : "Empty").append("\n");
             i++;
@@ -112,83 +146,117 @@ public class ChessClient {
     }
 
     private String joinGame(String[] params) throws Exception {
-        if (state == State.LOGGED_OUT) {
+        if (state != State.LOGGED_IN) {
             throw new Exception("You must be logged in to join a game.");
         }
         if (params.length != 2) {
             throw new Exception("Expected: play <ID> [WHITE|BLACK]");
         }
-        
+
         int listId;
         try {
             listId = Integer.parseInt(params[0]);
         } catch (NumberFormatException e) {
-            throw new Exception("ID must be a number from the list command.");
+            throw new Exception("ID must be a number from the list command.");     
         }
-        
+
         Integer gameId = gameIdMap.get(listId);
         if (gameId == null) {
             throw new Exception("Invalid game ID. Use the list command to see available games.");
         }
-        
-        String playerColor = params[1].toUpperCase();
-        if (!playerColor.equals("WHITE") && !playerColor.equals("BLACK")) {
+
+        String color = params[1].toUpperCase();
+        if (!color.equals("WHITE") && !color.equals("BLACK")) {
             throw new Exception("Player color must be WHITE or BLACK.");
         }
-        
-        facade.joinGame(authToken, new service.JoinGameRequest(playerColor, gameId));
-        
-        // Draw the board (Perspective: WHITE if color is WHITE, BLACK if color is BLACK)
-        return "Joined game " + listId + " as " + playerColor + ".\n" + drawBoard(playerColor);
+
+        facade.joinGame(authToken, new service.JoinGameRequest(color, gameId));
+
+        this.playerColor = color;
+        this.currentGameId = gameId;
+        this.ws = new WebSocketCommunicator(serverUrl, this);
+        this.ws.send(new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameId));
+        this.state = State.GAMEPLAY;
+
+        return "Joined game " + listId + " as " + color + ".";
     }
 
     private String observeGame(String[] params) throws Exception {
-        if (state == State.LOGGED_OUT) {
-            throw new Exception("You must be logged in to observe a game.");
+        if (state != State.LOGGED_IN) {
+            throw new Exception("You must be logged in to observe a game.");       
         }
         if (params.length != 1) {
             throw new Exception("Expected: observe <ID>");
         }
-        
+
         int listId;
         try {
             listId = Integer.parseInt(params[0]);
         } catch (NumberFormatException e) {
-            throw new Exception("ID must be a number from the list command.");
+            throw new Exception("ID must be a number from the list command.");     
         }
-        
+
         Integer gameId = gameIdMap.get(listId);
         if (gameId == null) {
             throw new Exception("Invalid game ID. Use the list command to see available games.");
         }
-        
-        // In Phase 5, observing doesn't require a server call since it doesn't change game state.
-        // We just draw the board from the white perspective.
-        return "Observing game " + listId + ".\n" + drawBoard("WHITE");
+
+        this.playerColor = null; // Observer perspective is WHITE
+        this.currentGameId = gameId;
+        this.ws = new WebSocketCommunicator(serverUrl, this);
+        this.ws.send(new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameId));
+        this.state = State.GAMEPLAY;
+
+        return "Observing game " + listId + ".";
     }
 
-    private String drawBoard(String perspective) {
-        return BoardDrawer.draw(perspective);
+    private String redrawBoard() throws Exception {
+        if (state != State.GAMEPLAY) {
+            throw new Exception("You are not in a game.");
+        }
+        return BoardDrawer.draw(playerColor != null ? playerColor : "WHITE");
+    }
+
+    private String leave() throws Exception {
+        if (state != State.GAMEPLAY) {
+            throw new Exception("You are not in a game.");
+        }
+        this.ws.send(new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, currentGameId));
+        this.state = State.LOGGED_IN;
+        this.currentGameId = null;
+        this.playerColor = null;
+        this.ws = null;
+        return "Left the game.";
     }
 
     public String help() {
         if (state == State.LOGGED_OUT) {
             return """
-                    register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                    register <USERNAME> <PASSWORD> <EMAIL> - to create an account  
                     login <USERNAME> <PASSWORD> - to play chess
                     quit - playing chess
                     help - with possible commands
                     """;
+        } else if (state == State.LOGGED_IN) {
+            return """
+                    create <NAME> - a game
+                    list - games
+                    play <ID> [WHITE|BLACK] - a game
+                    observe <ID> - a game
+                    logout - when you are done
+                    quit - playing chess
+                    help - with possible commands
+                    """;
+        } else {
+            return """
+                    redraw - the board
+                    leave - the game
+                    make <MOVE> - a move
+                    resign - the match
+                    highlight <PIECE> - legal moves
+                    help - with possible commands
+                    """;
         }
-        return """
-                create <NAME> - a game
-                list - games
-                play <ID> [WHITE|BLACK] - a game
-                observe <ID> - a game
-                logout - when you are done
-                quit - playing chess
-                help - with possible commands
-                """;
     }
 
     public State getState() {
